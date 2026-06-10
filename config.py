9860a2bbcb974728ae9df51a6a19092ce9198d13
Robert <adamyanoff@gmail.com>
2026-06-10 17:36:16 +0400

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
+print("DB_HOST =", repr(Settings().DB_HOST))
+print("DB_PORT =", repr(Settings().DB_PORT))
+print("PSYCOPG URL =", Settings().DATABASE_URL_psycopg)
 
 class Settings(BaseSettings):
     DB_HOST: str
